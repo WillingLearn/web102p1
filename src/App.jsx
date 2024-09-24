fdd2b3739616e8d108dac55198d431d42cb80d27
WillingLearn <34353944+WillingLearn@users.noreply.github.com>
2024-09-24 12:37:07 -0400

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Card from './Card'
 import './App.css'
 const App = () => {
   return (
+    <div>
+    <h1>Best Books of the 21st Century</h1>
     <div className="container" id='games-container'>
+      
       <Card title="The Sorcerer's Stone" img="hp1.jpg"genre="Fantasy" author="J.K. Rowling"/>
       <Card title="Chamber of Secrets"  img="hp2.jpg" genre="Fantasy" author="J.K. Rowling"/>
       <Card title="Prisoner of Azkaban" img="hp3.jpg" genre="Fantasy" author="J.K. Rowling"/>
@@ -12,6 +15,7 @@ const App = () => {
       <Card title="Harry Potter and the Half Blood Prince" img="hp6.jpg"genre="Fantasy" author="J.K. Rowling"/>
       <Card title="Harry Potter and the Deathly Hollows" img="hp7.jpg"genre="Fantasy" author="J.K. Rowling"/>
       
+    </div>
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div className='card'>
+        
         <div>{props.title}</div>
         {/* <img src={'./assets/${props.img}'}/> */}
         <img src={`/assets/${props.img}`} alt={props.title} />
